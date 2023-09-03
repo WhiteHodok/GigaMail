6a24bbcce687a5c48ae8a871bfd9751545c50bf2
Vladislav Mikhaylyuck <39564937+WhiteHodok@users.noreply.github.com>
2023-09-03 18:19:20 +0600

    WhiteHodok-SearchMail
    Добавлено :
    - Поиск e-mail
    - Админ-меню

--- buttons.py
+++ buttons.py
@@ -9,8 +9,26 @@ score = KeyboardButton('Статистика')
 uskmp.row(add)
 uskmp.row(score)
 
-# cancel markup
+# cancel markup user
 
 cancel_markup = InlineKeyboardMarkup(resize_keyboard=True)
 cancel = InlineKeyboardButton('Отмена', callback_data='cancel')
 cancel_markup.row(cancel)
+
+
+# cancel markup admin
+
+cancel_markup = InlineKeyboardMarkup(resize_keyboard=True)
+cancel = InlineKeyboardButton('Отмена', callback_data='cancel_admin')
+cancel_markup.row(cancel)
+
+# admin keyboard markup
+adkmp = ReplyKeyboardMarkup(resize_keyboard=True)
+search = KeyboardButton('Поиск почты')
+load = KeyboardButton('Получить почты')
+rules = KeyboardButton('Права')
+back = KeyboardButton('Назад')
+adkmp.row(search)
+adkmp.row(load)
+adkmp.row(rules)
+adkmp.row(back)
